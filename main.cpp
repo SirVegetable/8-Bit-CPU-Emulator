b1916e746fa26ae0c9ce67595ab596e6adfc45d9
SirVegetable <17wgs@queensu.ca>
2023-12-06 21:17:45 -0500

    added a LDA flag setter and a readByte function that doesn't increment the programcounter

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ typedef uint8_t Byte;
 typedef uint16_t Rock;
 
 struct Memory{
-    static constexpr std::size_t maxMemorySize = 64 * 1024; // 64 kb
+    static constexpr std::size_t maxMemorySize = 64 * 1024; // 64Kb
 
     std::array<Byte,maxMemorySize> data; 
 
@@ -15,11 +15,11 @@ struct Memory{
             data[i] = 0; 
         }
     }
-    //Read one byte of memory, may get rid of later for a read and write function
-    Byte operator [](unsigned int Address) const{
-        //must assert the address is less than maxMemorySize
-        return data[Address];
-    }
+    // //Read one byte of memory, may get rid of later for a read and write function
+    // Byte operator [](unsigned int Address) const{
+    //     //must assert the address is less than maxMemorySize
+    //     return data[Address];
+    // }
 };
 
 
@@ -42,14 +42,14 @@ struct CPU{
     Byte zero : 1;
     Byte interrDisable : 1; 
     Byte decMode : 1; 
-    Byte brCmd : 1; 
+    Byte breakCmd : 1; 
     Byte overflow : 1; 
     Byte neg: 1; 
     
     /* Emulates the computer turning off and the CPU having to reset*/
     void Reset(Memory &mem){
-        carry = 0, zero = 0, interrDisable = 0, decMode = 0, brCmd = 0, overflow = 0, neg =0;
-        ProgramCounter = 0xFFFC;
+        carry = 0, zero = 0, interrDisable = 0, decMode = 0, breakCmd = 0, overflow = 0, neg =0;
+        ProgramCounter = 0xFFFC; // reset location
         StackPointer = 0x0100;
 
         mem.initializeMem();
@@ -62,6 +62,18 @@ struct CPU{
 
         return data; 
     }
+    //function to read a byte through a CPU cycle but doesn't increment PC like fetch
+    Byte ReadByte(Memory &mem,unsigned int& Cycles){
+        Byte Data = mem.data[ProgramCounter];
+        Cycles--; 
+        return Data; 
+    }
+    void LDA_FLAG_SETTR(){
+        zero = (Accum == 0);
+        //binary literal used to check if accum's 7th bit is set
+        neg = (Accum& 0b10000000) > 0; 
+
+    }
     //opcodes
     static constexpr Byte LDA_IM_INS = 0xA89, LDA_ZP_INS = 0xA5; 
 
@@ -69,19 +81,25 @@ struct CPU{
     to carry out the instructions*/
     
     
-    void execute(Memory &mem, unsigned int cycles){
+    void execute(Memory &mem, unsigned int Cycles){
 
-        while (cycles != 0){
-            Byte instructions = fetch(mem, cycles);
+        while (Cycles != 0){
+            Byte instructions = fetch(mem, Cycles);
             switch (instructions)
             {
             case LDA_IM_INS:{
-                Byte Value = fetch(mem,cycles);
-                Accum = Value; 
-                zero = (Accum == 0);
-                neg = (Accum & 0b10000000) > 0; 
+
+                Byte secondByte = fetch(mem,Cycles);
+                Accum = secondByte;
+                LDA_FLAG_SETTR();
+
+            }break;
+
+            case LDA_ZP_INS:{
+                Byte ZPaddress = fetch(mem,Cycles);
+
+
             }
-                break;
             
             default:{
                 std::cout << "the instructions could not be handled\n";
@@ -108,6 +126,8 @@ int main(){
     CPU cpu;
     cpu.Reset(mem);
     cpu.execute(mem,4);
+    mem.data[0xFFFC] = CPU::LDA_IM_INS; 
+    mem.data[0xFFFD] = 0x42; 
 
 
     return 0; 
