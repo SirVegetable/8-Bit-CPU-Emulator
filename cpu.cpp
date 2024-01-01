fc2af0bdc667660ef9147b0ae9d6c51fead09f62
SirVegetable <17wgs@queensu.ca>
2024-01-01 15:34:23 -0500

    added header file for typedefs used across files

--- cpu.cpp
+++ cpu.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <array>
- 
-//Definitions for 8 bit type and 16 bit type 
-typedef uint8_t Byte; 
-typedef uint16_t Rock;
+#include "typedefs.h"
+
 
 
 
---
+++ typedefs.h
@@ -0,0 +1,8 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+#include <cstdint>
+//Definitions for 8 bit type and 16 bit type 
+typedef uint8_t Byte; 
+typedef uint16_t Rock;
+
+#endif
