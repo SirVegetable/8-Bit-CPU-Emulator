a53fe3e25f7effd9f2591895bc30324366c61c58
SirVegetable <17wgs@queensu.ca>
2024-01-01 15:16:28 -0500

    adding files for my memory implementation

---
+++ mem.cpp
@@ -0,0 +1,37 @@
+#include "mem.hpp"
+
+#include<array> 
+#include<iostream>
+
+class Memory{
+public:
+        //constructor
+    Memory::Memory()
+    {
+    }
+        //destructor
+    Memory::~Memory()
+    {
+    }
+        //copy assignment operator
+
+    void Memory::initializeMem(){
+        for(size_t i = 0; i < maxMemSize; i++){
+
+        }
+    };
+
+    private: 
+        
+
+
+
+
+
+
+    
+    
+
+
+};
+
