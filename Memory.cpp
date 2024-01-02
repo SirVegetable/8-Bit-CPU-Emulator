26ad28fb4c01a39538b17e2d8c165667341e0fe6
SirVegetable <17wgs@queensu.ca>
2024-01-02 17:23:06 -0500

    added a read function to the our memory class that we can use later on in our BUS and CPU classes without having to use inheritance

--- Memory.cpp
+++ Memory.cpp
@@ -15,6 +15,15 @@ void Memory::initializeMem(){
     for(size_t i = 0; i < MaxMemSize; i++){
         data[i] = 0; 
     }
+
+}
+Byte Memory::read(Rock addr){
+        if(addr >= 0x0000 && addr <= 0xFFFF ){
+                return data[addr];
+        }
+        else{
+                return 0x00; 
+        }
 }
 
 
--- Memory.hpp
+++ Memory.hpp
@@ -1,22 +1,26 @@
 
 #ifndef MEMORY_HPP
 #define MEMORY_HPP
-
-#pragma once
-
+#include <iostream>
+#include <array>
 #include "typedefs.h"
-#include <iostream> 
-class Memory {
-public:
-    static const size_t MaxMemSize = 64 * 1024;
 
-    Memory();
+class Memory{
+public:
+    Memory(); 
+    
     ~Memory();
 
+    //Initialize Memory function
     void initializeMem();
 
-private:
-    Byte data[MaxMemSize];
+    //Read and Write functions
+    Byte read(Rock addr);
+    void write();
+
+    //member values 
+    static constexpr std::size_t MaxMemSize = 64 * 1024;
+    std::array<Byte,MaxMemSize> data; 
 };
 
 #endif
