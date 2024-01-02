3a282dbcff5e6fae4e0e2f90efd32077c28ce3a8
SirVegetable <17wgs@queensu.ca>
2024-01-02 18:06:10 -0500

    interface for my Bus class

--- bus.hpp
+++ bus.hpp
@@ -1,9 +1,21 @@
 #include "Memory.hpp"
 #include "cpu.hpp"
+#ifndef BUS_HPP
+#define BUS_HPP
 class Bus
-{
+{   
+    //Constructor && Destructor
+    public: 
+        Bus(); 
+        ~Bus();
+
+    //Member functions
+    void write();
+    Byte read(); 
+    //Members     
     public:
         Memory Mem;
         CPU cpu; 
 
 };
+#endif 
